848981c79f2d419dc71a69ec892666d0f5440e40
max-student@outlook.com <nicholasbaker7010@gmail.com>
2019-09-03 11:58:01 -0400

    First Commit

---
+++ GeometricShapesLibrary/Square.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricShapesLibrary {
+   public class Square : Parallelegram {
+        public Square(int sides) :base(sides, sides) {
+
+        }
+    }
+}
